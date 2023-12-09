1c137cbd1d7bdfe3f448b4bbf2aa136f2555f9be
imdeepakvermaa <dv24012002@gmail.com>
2023-12-09 22:59:40 +0530

    html content and added the files

--- Small projects/bg changer/script.js
+++ Small projects/bg changer/script.js
@@ -2,7 +2,7 @@ function changeColor(color){
     document.body.style.background = color;
 
 }
-
+web 
 function myFunc(){
     var colors = ['red', 'green', 'blue', 'yellow', 'orange'];
     var randomIndex = Math.floor(Math.random() * colors.length);

a135e97f8845017881d24a2a6cdf7a9b9c9a1feb
imdeepakvermaa <dv24012002@gmail.com>
2023-12-09 14:09:35 +0530

    background changer done

---
+++ Small projects/script.js
@@ -0,0 +1,11 @@
+function changeColor(color){
+    document.body.style.background = color;
+
+}
+
+function myFunc(){
+    var colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+    var randomIndex = Math.floor(Math.random() * colors.length);
+    var randomColor = colors[randomIndex];
+    changeColor(randomColor);
+}
